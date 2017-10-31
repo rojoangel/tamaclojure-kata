a57b16011deaa56d8f0d60cc42c08a29552c18f1
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-31 18:48:25 +0100

    I have a Tamagotchi

--- test/tamagotchi/core_test.clj
+++ test/tamagotchi/core_test.clj
@@ -3,5 +3,12 @@
             [tamagotchi.core :refer :all]
             [spexec.core :refer :all]))
 
+(defn born []
+  {})
+
+(defgiven #"I have a Tamagotchi"
+          [_]
+          (born))
+
 ;; run specs
 (exec-spec "resources/tamagotchi.feature")
