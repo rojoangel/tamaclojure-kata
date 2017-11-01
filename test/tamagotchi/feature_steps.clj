9365e81e0db8f6c86c5c8b63253041f14ae464cd
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-01 11:56:36 +0100

    require core as tamagotchi

--- test/tamagotchi/feature_steps.clj
+++ test/tamagotchi/feature_steps.clj
@@ -1,26 +1,26 @@
 (ns tamagotchi.feature-steps
   (:require [clojure.test :refer :all]
             [spexec.core :refer :all]
-            [tamagotchi.core :refer :all]))
-
-(def initial-hungriness 50)
+            [tamagotchi.core :as tamagotchi]))
 
 (defgiven #"I have a Tamagotchi"
           [_]
-          (born))
+          (tamagotchi/born))
 
 (defwhen #"I feed it"
          [tamagotchi]
-         (feed tamagotchi))
+         (tamagotchi/feed tamagotchi))
 
 (defthen #"its hungriness is decreased"
          [tamagotchi]
          (do
-           (is (= (dec default-hungriness) (:hungriness tamagotchi)))
+           (is (= (dec tamagotchi/default-hungriness)
+                  (:hungriness tamagotchi)))
            tamagotchi))
 
 (defwhen #"its fullness is increased"
          [tamagotchi]
          (do
-           (is (= (inc default-fullness) (:fullness tamagotchi)))
+           (is (= (inc tamagotchi/default-fullness)
+                  (:fullness tamagotchi)))
            tamagotchi))
